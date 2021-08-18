fce1df9c1cee899c215c909ae5ccfece7db16e84
Ghostleader1399 <88676150+Ghostleader1399@users.noreply.github.com>
2021-08-18 10:44:34 -0400

    Update Delete.php

--- src/RedCraftPE/RedSkyBlock/Commands/SubCommands/Delete.php
+++ src/RedCraftPE/RedSkyBlock/Commands/SubCommands/Delete.php
@@ -6,55 +6,32 @@ use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat;
 use pocketmine\Player;
 
-class Delete {
+class DisbandCommand extends IslandCommand {
 
-  public function __construct($plugin) {
+    /** @var IslandManager */
+    private $islandManager;
 
-    $this->plugin = $plugin;
-  }
-
-  public function onDeleteCommand(CommandSender $sender, array $args): bool {
-
-    if ($sender->hasPermission("redskyblock.delete")) {
-
-      if (count($args) < 2) {
-
-        $sender->sendMessage(TextFormat::WHITE . "Usage: /is delete <player>");
-        return true;
-      } else {
-
-        $playerName = strtolower(implode(" ", array_slice($args, 1)));
-        $plugin = $this->plugin;
-        $player = $plugin->getServer()->getPlayerExact($playerName);
-        $skyblockArray = $plugin->skyblock->get("SkyBlock", []);
-
-        if ($player instanceof Player) {
-
-          $player->sendMessage(TextFormat::RED . "Your island has been deleted by a server administrator.");
-          $player->teleport($plugin->getServer()->getDefaultLevel()->getSafeSpawn());
-        }
-
-        if (array_key_exists($playerName, $skyblockArray)) {
-
-          $filePath = $plugin->getDataFolder() . "Players/" . $playerName . ".json";
-
-          unset($skyblockArray[$playerName]);
-          unlink($filePath);
-
-          $plugin->skyblock->set("SkyBlock", $skyblockArray);
-          $plugin->skyblock->save();
-          $sender->sendMessage(TextFormat::GREEN . "You have successfully deleted " . TextFormat::WHITE . $playerName . "'s" . TextFormat::GREEN . " island.");
-          return true;
-        } else {
-
-          $sender->sendMessage(TextFormat::WHITE . $playerName . TextFormat::RED . " does not have an island to delete.");
-          return true;
-        }
-      }
-    } else {
-
-      $sender->sendMessage(TextFormat::RED . "You don't have permission to use this command.");
-      return true;
+    public function __construct(IslandCommandMap $map) {
+        $this->islandManager = $map->getPlugin()->getIslandManager();
     }
-  }
+
+    public function getName(): string {
+        return "disband";
+    }
+
+    public function getUsageMessageContainer(): MessageContainer {
+        return new MessageContainer("DISBAND_USAGE");
+    }
+
+    public function getDescriptionMessageContainer(): MessageContainer {
+        return new MessageContainer("DISBAND_DESCRIPTION");
+    }
+
+    public function onCommand(Session $session, array $args): void {
+        if($this->checkFounder($session)) {
+            return;
+        }
+        IslandFactory::disbandIsland($session->getIsland());
+    }
+
 }
